f1c9aff1565be62e50adfbd556f99d1e42809beb
Subeen <glitterbeen99@gmail.com>
2023-08-20 16:17:27 +0900

    14425번 solve

--- 01_sort_map_set/14425.cpp
+++ 01_sort_map_set/14425.cpp
@@ -3,11 +3,39 @@
 //
 
 #include <iostream>
-#include <string>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, m, count=0;
+    cin >> n >> m;
+
+    vector<string> v1(n);
+    vector<string> v2(m);
+
+    // 입력
+    for (int i = 0; i < n; ++i) {
+        cin >> v1[i];
+    }
+    for (int i = 0; i < m; ++i) {
+        cin >> v2[i];
+    }
+    sort(v1.begin(), v1.end());
+
+    for (int i = 0; i < v2.size(); ++i) {
+        if(binary_search(v1.begin(), v1.end(), v2[i]))
+            count++;
+        else continue;
+    }
+
+    // 출력
+    cout << count;
+
+
     return 0;
 }
